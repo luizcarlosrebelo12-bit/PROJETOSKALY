0d1ac03e241e2358eea4d56703eb3bf2bca683ec
t06082625-lang <t06082625@gmail.com>
2026-09-17 16:15:58 -0300

    att

--- lib/google-form.ts
+++ lib/google-form.ts
@@ -3,6 +3,9 @@ const FORM_BASE_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfgqyEROIK7Ihd0x
 const PROJECT_ID_ENTRY = 'entry.1005265204'
 
 export function getFormularioLink(projectId: string) {
-  const params = new URLSearchParams({ [PROJECT_ID_ENTRY]: projectId })
+  const params = new URLSearchParams({
+    usp: 'pp_url',
+    [PROJECT_ID_ENTRY]: projectId,
+  })
   return `${FORM_BASE_URL}?${params.toString()}`
 }
